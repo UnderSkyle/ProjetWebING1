42db2347c4e3633552b0a5299e870f80101f3a8a
Fiona <cleo9501@gmail.com>
2024-02-05 15:58:16 +0100

    card changes

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -1,18 +1,30 @@
 import './Card.css'
-import '../assets/'
+import image from '../assets/nourriture1.png'
 import {useState} from "react";
 
 function Card(){
+    const [count, setCount] = useState(0);
+
+    const increment = () => {
+        setCount(count+1);
+    }
+
+    const decrement = () => {
+        setCount(count-1);
+    }
+
     return(
         <div className="card">
             <h2 className="produit">produit</h2>
             <h4>N° ref</h4>
-            <img className="card-image" src=""/>
-            <div className="compteur">
-                
-
+            <img className="card-image" src={image}/>
+            <div className="compteur-div">
+                <p className="compt">{count}</p>
+                <button className="compt-bouton" onClick={decrement}>-</button>
+                <button className="compt-bouton" onClick={increment}>+</button>
             </div>
             <img/>
+
             <h2>&euro;</h2>
             <h4>Stock : </h4>
         </div>
