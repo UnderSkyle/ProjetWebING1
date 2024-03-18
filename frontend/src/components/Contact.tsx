70ed6e31965ab482bf0b1bf36b0bc156581f2509
Fiona <cleo9501@gmail.com>
2024-03-18 16:18:21 +0100

    Login and Header

--- frontend/src/components/Contact.tsx
+++ frontend/src/components/Contact.tsx
@@ -26,129 +26,132 @@ function Contact() {
 
     return(
         <>
-        <br/><br/><br/>
-        <div className="first_container">
-            <h1 className="text">Contactez nous !</h1>
-            <h2 className="text">Si vous avez une question, remplissez ce formulaire et nous vous répondrons en moins de 48h.</h2>
-        </div>
-        <div className="container">
-                
-            <form onSubmit = {handleSubmit}>
-                <div className="form_row">
-                    <div className="input_data">
-                        <input required type="text" 
-                            name="prenom"
-                            value={inputs.prenom || ""}
-                            onChange={handleChange}/>
-                        <div className="underline"></div>
-                        <label for="">Prénom</label>
-                    </div>
-
-
-                    <div className="input_data">
-                        <input required type="text"
-                            name="nom"
-                            value={inputs.nom || ""}
-                            onChange={handleChange}/>
-                            <div className="underline"></div>
-                            <label for="">Nom</label>
-                    </div>
-                </div>
-
-
-                <div className="form_row">
-                    <div className="input_data">
-                        <input required type="email"
-                            name="email"
-                            value={inputs.email || ""}
-                            onChange={handleChange}/>
-                            <div className="underline"></div>
-                            <label for="">Email</label>
-                    </div>
-                </div>
-
-
-                <div className="form_row">
-                    <div className="radio">
-                        Genre<br/><br/>
-                        <label>
-                        <input type="radio" className="input_radio"
-                            name="genre"
-                            value="Femme"
-                            checked = {inputs.genre == "Femme"}
-                            onChange = {handleChange}
-                            required/>
-                            Femme</label>
-
-                        <label>
-                        <input type="radio" className="input_radio"
-                            name="genre"
-                            value="Homme"
-                            checked = {inputs.genre == "Homme"}
-                            onChange = {handleChange}/>
-                            Homme</label>    
-
-                        <label> 
-                        <input type="radio" className="input_radio"
-                            name="genre"
-                            value="Autre"
-                            checked = {inputs.genre == "Autre"}
-                            onChange = {handleChange}/>
-                            Autre</label> 
-                    </div> 
-                </div>      
-                <div className="form_row">
-                    <div className="select">Métier<br/><br/>
-                        <select name = "metier" value = {inputs.metier} onChange = {handleChange}>
-                            <option value="enseignant">Enseignant</option>
-                            <option value="agriculteur">Agriculteur</option>
-                            <option value="medecin">Médecin</option>
-                            <option value="cadre">Cadre</option>
-                        </select>
-                    </div>
-                    <div className="date">
-                        Date<label><br/><br/>
-                            <input required type = "date"
-                                            name = "date"
-                                            value = {inputs.date}/>
-                        </label>
-                    </div>
-                </div>
-
-
-                <div className="form_row">
-                    <div className="input_data textarea">
-                        <textarea required name="obj" rows="5" cols="8" 
-                            value={inputs.obj || ""}
-                            onChange={handleChange}/>
-                        <br/>
-                        <div className="underline"></div>
-                        <label for="">Objet du message</label>
-                        <br/>
-                    </div>
-                </div>
-                
-
-                <div className="form_row">
-                    <div className="input_data textarea">
-                        <textarea required name="mess" rows="5" cols="8"
-                                    value={inputs.mess || ""}
-                                    onChange={handleChange}/>
-                        <br/>
-                        <div className="underline"></div>
-                        <label for="">Contenu du message</label>
-                        <br/>   
-                    </div>
-               </div>
-               <div className="form_row submit_btn">
-                    <div className="input_data">
-                        <div className="inner"></div>
-                        <input type="submit" value="submit"/>
-                    </div>
-                </div>
-            </form>
+        <div className="big_container">
+            <br/><br/><br/>
+            <div className="first_container">
+                <h1 className="text">Contactez nous !</h1>
+                <h2 className="text">Si vous avez une question, remplissez ce formulaire et nous vous répondrons en moins de 48h.</h2>
             </div>
-            <br/><br/>
+            <div className="container">
+                    
+                <form onSubmit = {handleSubmit}>
+                    <div className="form_row">
+                        <div className="input_data">
+                            <input required type="text" 
+                                name="prenom"
+                                value={inputs.prenom || ""}
+                                onChange={handleChange}/>
+                            <div className="underline"></div>
+                            <label for="">Prénom</label>
+                        </div>
+
+
+                        <div className="input_data">
+                            <input required type="text"
+                                name="nom"
+                                value={inputs.nom || ""}
+                                onChange={handleChange}/>
+                                <div className="underline"></div>
+                                <label for="">Nom</label>
+                        </div>
+                    </div>
+
+
+                    <div className="form_row">
+                        <div className="input_data">
+                            <input required type="email"
+                                name="email"
+                                value={inputs.email || ""}
+                                onChange={handleChange}/>
+                                <div className="underline"></div>
+                                <label for="">Email</label>
+                        </div>
+                    </div>
+
+
+                    <div className="form_row">
+                        <div className="radio">
+                            Genre<br/><br/>
+                            <label>
+                            <input type="radio" className="input_radio"
+                                name="genre"
+                                value="Femme"
+                                checked = {inputs.genre == "Femme"}
+                                onChange = {handleChange}
+                                required/>
+                                Femme</label>
+
+                            <label>
+                            <input type="radio" className="input_radio"
+                                name="genre"
+                                value="Homme"
+                                checked = {inputs.genre == "Homme"}
+                                onChange = {handleChange}/>
+                                Homme</label>    
+
+                            <label> 
+                            <input type="radio" className="input_radio"
+                                name="genre"
+                                value="Autre"
+                                checked = {inputs.genre == "Autre"}
+                                onChange = {handleChange}/>
+                                Autre</label> 
+                        </div> 
+                    </div>      
+                    <div className="form_row">
+                        <div className="select">Métier<br/><br/>
+                            <select name = "metier" value = {inputs.metier} onChange = {handleChange}>
+                                <option value="enseignant">Enseignant</option>
+                                <option value="agriculteur">Agriculteur</option>
+                                <option value="medecin">Médecin</option>
+                                <option value="cadre">Cadre</option>
+                            </select>
+                        </div>
+                        <div className="date">
+                            Date<label><br/><br/>
+                                <input required type = "date"
+                                                name = "date"
+                                                value = {inputs.date}/>
+                            </label>
+                        </div>
+                    </div>
+
+
+                    <div className="form_row">
+                        <div className="input_data textarea">
+                            <textarea required name="obj" rows="5" cols="8" 
+                                value={inputs.obj || ""}
+                                onChange={handleChange}/>
+                            <br/>
+                            <div className="underline"></div>
+                            <label for="">Objet du message</label>
+                            <br/>
+                        </div>
+                    </div>
+                    
+
+                    <div className="form_row">
+                        <div className="input_data textarea">
+                            <textarea required name="mess" rows="5" cols="8"
+                                        value={inputs.mess || ""}
+                                        onChange={handleChange}/>
+                            <br/>
+                            <div className="underline"></div>
+                            <label for="">Contenu du message</label>
+                            <br/>   
+                        </div>
+                </div>
+                <div className="form_row submit_btn">
+                        <div className="input_data">
+                            <div className="inner"></div>
+                            <input type="submit" value="submit"/>
+                        </div>
+                    </div>
+                </form>
+                
+                </div>
+                <br/><br/><br/><br/><br/><br/>
+            </div>
         </>
     )
 }
--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -19,45 +19,46 @@ function Login() {
 
     return(
         <>
-        <br/><br/><br/>
-        <div className="container">
-            <h1 className="text">Connexion</h1>
-            <form onSubmit = {handleSubmit}>
-                <div className="form_row">
-                    <div className="input_data">
-                        <input required type="email"
-                            name="email"
-                            value={inputs.email || ""}
-                            onChange={handleChange}/>
+        <div className="big_container">
+            <div className="container">
+                <h1 className="text">Connexion</h1>
+                <form onSubmit = {handleSubmit}>
+                    <div className="form_row">
+                        <div className="input_data">
+                            <input required type="email"
+                                name="email"
+                                value={inputs.email || ""}
+                                onChange={handleChange}/>
+                                <div className="underline"></div>
+                                <label for="">Email</label>
+                        </div>
+                    </div>
+
+                    <div className="form_row">
+                        <div className="input_data">
+                            <input required type="text" 
+                                name="mdp"
+                                value={inputs.mdp || ""}
+                                onChange={handleChange}/>
                             <div className="underline"></div>
-                            <label for="">Email</label>
+                            <label for="">Mot de passe</label>
+                        </div>
                     </div>
-                </div>
-
-                <div className="form_row">
-                    <div className="input_data">
-                        <input required type="text" 
-                            name="mdp"
-                            value={inputs.mdp || ""}
-                            onChange={handleChange}/>
-                        <div className="underline"></div>
-                        <label for="">Mot de passe</label>
-                    </div>
-                </div>
 
 
+                    
+                    <p>Vous n'avez pas de compte ? Créez-en un !</p>
                 
-                <p>Vous n'avez pas de compte ? Créez-en un !</p>
-            
-            <div className="form_row submit_btn">
-                    <div className="input_data">
-                        <div className="inner"></div>
-                        <input type="submit" value="submit"/>
+                <div className="form_row submit_btn">
+                        <div className="input_data">
+                            <div className="inner"></div>
+                            <input type="submit" value="submit"/>
+                        </div>
                     </div>
+                </form>
                 </div>
-            </form>
+                <br/><br/>
             </div>
-            <br/><br/>
             
         </>
     )
