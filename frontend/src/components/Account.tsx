e0809e47af7a17f22f886b4e83107c7e5048acd9
Fiona <cleo9501@gmail.com>
2024-04-29 11:45:43 +0200

    html css

--- frontend/src/components/Account.tsx
+++ frontend/src/components/Account.tsx
@@ -14,31 +14,41 @@ function Account() {
         <link rel="presonnect" href="https://fonts.googleapis.com"/>
         <link rel="presonnect" href="https://fonts.gstatic.com"/>
         <br/><br/><br/>
-            <div>
-                <h1>Votre compte</h1>
-                <a href="#blocks-know-more"><button className="LinkButton" role="button">Se déconnecter</button></a>
-           </div>
+            <div className="mediumcontainer-account">
+                <h1 className="one">Votre compte</h1>
+                <a href="#blocks-know-more" className="two"><button className="LinkButton-account" role="button">Se déconnecter</button></a>
+            </div>
 
-           <div className="bigcontainer">
-                <div className="container">
-                    <span className="material-symbols-outlined-card material-symbols-outlined icon">account_circle
-                        <h6>Votre profil</h6>
-                        <p>Voir les informations et les modifier</p>
-                    </span>
+           <div className="bigcontainer-account">
+                <div className="container-account one">
+                    <div className="grid1-account">
+                        <span className="material-symbols-outlined-card material-symbols-outlined icon-account">account_circle</span>
+                    </div>
+                    <div className="grid2-account"><h6>Votre profil</h6></div>
+                    <div className="grid3-account"><p>Voir les informations et les modifier</p></div>    
+                    <br/>
+                        
+                    
                 </div>
 
-                <div className="container">
-                    <span className="material-symbols-outlined-card material-symbols-outlined icon">home
-                        <h4>Vos adresses</h4>
-                        <p>Voir vos adresses de livraison et les modifier</p>
-                    </span>
+                <div className="container-account two">
+                    <div className="grid1-account">
+                        <span className="material-symbols-outlined-card material-symbols-outlined icon-account">home</span>
+                    </div>    
+                    <div className="grid2-account"><h6>Vos adresses</h6></div>
+                    <br/>
+                    <div className="grid3-account"><p>Voir vos adresses de livraison et les modifier</p></div>
+                    
                 </div>
 
-                <div className="container">
-                    <span className="material-symbols-outlined-card material-symbols-outlined icon">package_2
-                        <h4>Vos commandes</h4>
-                        <p>Voir vos commandes et les suivre</p>
-                    </span>
+                <div className="container-account three">
+                    <div className="grid1-account">
+                        <span className="material-symbols-outlined-card material-symbols-outlined icon-account">package_2</span>
+                    </div>     
+                    <div className="grid2-account"><h6>Vos commandes</h6></div>
+                        <br/>
+                    <div className="grid3-account"><p>Voir vos commandes et les suivre</p></div>
+                    
                 </div>
                 <br/><br/><br/><br/><br/><br/><br/><br/><br/>
            </div>
