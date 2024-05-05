9ef15ba853e5f82d7500fe2e6b4c781f18135333
Alexandra Baivel <abaivel@outlook.fr>
2024-05-06 00:05:43 +0200

    protection api + finition front

--- backend/posts/views.py
+++ backend/posts/views.py
@@ -15,7 +15,7 @@ from django.utils import timezone
 
 @csrf_exempt
 @api_view(['POST'])
-def send_mail_contact(request:dict):
+def send_mail_contact(request):
     """request={
         surname:
         name:
@@ -27,10 +27,10 @@ def send_mail_contact(request:dict):
         message:
     }"""
     if (request.method == "POST"):
-        data = request.data
-        message = f"Mail de %s, %s %s (%s), %s, né(e) le %s :\n %s" % (data.get("email"), data.get("surname"),data.get("name"), data.get("gender"), data.get("job"), data.get("birthdate"), data.get("message"))
-        print("ici")
         try:
+            data = request.data
+            message = f"Mail de %s, %s %s (%s), %s, né(e) le %s :\n %s" % (data.get("email"), data.get("surname"),data.get("name"), data.get("gender"), data.get("job"), data.get("birthdate"), data.get("message"))
+            print("ici")
             print("ici2")
             send_mail(
                 subject="test",
@@ -86,7 +86,6 @@ def create_account(request):
             return Response(status=status.HTTP_400_BAD_REQUEST)
         except User.DoesNotExist: 
             user = User.objects.create_user(username=email,email=email,password=pwd,first_name=name,last_name=surname)
-
             return Response(user.id,status=status.HTTP_200_OK)
 
 @csrf_exempt
@@ -167,15 +166,16 @@ def delete_address(request):
             id_address
         }"""
     if (request.method=="DELETE"):
-        data=request.data
-        user = User.objects.get(id=data.get("id_user"))
-        address = Address.objects.get(id=data.get("id_address"))
-        if (address.user!=user):
+        try:
+            data=request.data
+            user = User.objects.get(id=data.get("id_user"))
+            address = Address.objects.get(id=data.get("id_address"))
+            if (address.user!=user):
+                return Response(status=status.HTTP_401_UNAUTHORIZED)
+            address.delete()
+            return Response(status=status.HTTP_200_OK)
+        except User.DoesNotExist or Address.DoesNotExist:
             return Response(status=status.HTTP_400_BAD_REQUEST)
-        address.delete()
-        return Response(status=status.HTTP_200_OK)
-
-
 
 @csrf_exempt
 @api_view(['POST'])
@@ -190,11 +190,14 @@ def create_address(request):
             complementary_info
         }"""
     if (request.method=="POST"):
-        data=request.data
-        user=User.objects.get(id=data.get("id_user"))
-        user.address_set.create(first_name=data.get("first_name"),last_name=data.get("last_name"),street=data.get("street"), postal_code=data.get("postal_code"), city=data.get("city"), complementary_info=data.get("complementary_info"))
-        user.save()
-        return Response(status=status.HTTP_200_OK)
+        try:
+            data=request.data
+            user=User.objects.get(id=data.get("id_user"))
+            user.address_set.create(first_name=data.get("first_name"),last_name=data.get("last_name"),street=data.get("street"), postal_code=data.get("postal_code"), city=data.get("city"), complementary_info=data.get("complementary_info"))
+            user.save()
+            return Response(status=status.HTTP_200_OK)
+        except User.DoesNotExist:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
 
 @csrf_exempt
 @api_view(['PUT'])
@@ -210,61 +213,73 @@ def update_address(request):
             complementary_info
         }"""
     if (request.method=="PUT"):
-        data=request.data
-        address = Address.objects.get(id=data.get("id_address"))
-        address.first_name = data.get("first_name")
-        address.last_name = data.get("last_name")
-        address.street = data.get("street")
-        address.postal_code = data.get("postal_code")
-        address.city = data.get("city")
-        address.complementary_info = data.get("complementary_info")
-        address.save()
-        return Response(status=status.HTTP_200_OK)
+        try:
+            data=request.data
+            address = Address.objects.get(id=data.get("id_address"))
+            if address.user.id!=data.get("id_user"):
+                return Response(status=status.HTTP_401_UNAUTHORIZED)
+            address.first_name = data.get("first_name")
+            address.last_name = data.get("last_name")
+            address.street = data.get("street")
+            address.postal_code = data.get("postal_code")
+            address.city = data.get("city")
+            address.complementary_info = data.get("complementary_info")
+            address.save()
+            return Response(status=status.HTTP_200_OK)
+        except Address.DoesNotExist:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
 
 @csrf_exempt
 @api_view(['GET'])
 def get_addresses(request):
-     """id_user
+    """id_user
             first_name
             last_name
             street
             postal_code
             city
             complementary_info"""
-     try:
-         user_id = request.GET.get('user_id')
-         if user_id is None:
-             return Response(status=400)
-         user = User.objects.get(id=user_id)
-         addresses = Address.objects.filter(user=user)
-         addresses_data = [
-              {'id':address.id,'first_name': address.first_name, 'last_name': address.last_name, 'street': address.street, 'postal_code': address.postal_code, 'city': address.city, "complementary_info": address.complementary_info}
-               for address in addresses
+    try:
+        user_id = request.GET.get('user_id')
+        if user_id is None:
+            return Response(status=400)
+        try:
+            user = User.objects.get(id=user_id)
+        except User.DoesNotExist:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        addresses = Address.objects.filter(user=user)
+        addresses_data = [
+            {'id':address.id,'first_name': address.first_name, 'last_name': address.last_name, 'street': address.street, 'postal_code': address.postal_code, 'city': address.city, "complementary_info": address.complementary_info}
+            for address in addresses
          ]
-         return Response(addresses_data, status=status.HTTP_200_OK)
-     except Address.DoesNotExist:
-         return Response(status=404)
+        return Response(addresses_data, status=status.HTTP_200_OK)
+    except Address.DoesNotExist:
+        return Response(status=404)
 
 @csrf_exempt
 @api_view(['GET'])
 def get_address(request):
-     try:
-         id_address = request.GET.get('idAddress')
-         if  id_address is None:
-             return Response(status=400)
-         address = Address.objects.get(id=id_address)
-         address_data = {'id':address.id,'prenom': address.first_name, 'nom': address.last_name, 'adresse': address.street, 'codePostal': address.postal_code, 'ville': address.city, "complement": address.complementary_info}
-         print(address_data)
-         return Response(address_data, status=status.HTTP_200_OK)
-     except Address.DoesNotExist:
-         return Response(status=status.HTTP_200_OK)
-
-
+    try:
+        id_address = request.GET.get('idAddress')
+        if  id_address is None:
+            return Response(status=400)
+        try:
+            address = Address.objects.get(id=id_address)
+        except Address.DoesNotExist:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        address_data = {'id':address.id,'prenom': address.first_name, 'nom': address.last_name, 'adresse': address.street, 'codePostal': address.postal_code, 'ville': address.city, "complement": address.complementary_info}
+        print(address_data)
+        return Response(address_data, status=status.HTTP_200_OK)
+    except Address.DoesNotExist:
+        return Response(status=status.HTTP_200_OK)
 
 @csrf_exempt
 @api_view(['POST'])
 def remove_item(request):
-    print(request.data)
+    """request={
+        user_id
+        product_ref
+    }"""
     if request.method == 'POST':
         try:
             user_id = request.data.get('user_id')
@@ -278,99 +293,110 @@ def remove_item(request):
             cart_item = CartItem.objects.get(cart=cart, product=product)
             cart_item.delete()
 
-            return Response(status=200)
+            return Response(status=status.HTTP_200_OK)
 
         except Cart.DoesNotExist:
-            print('noCart')
-            return Response(status=404)
+            return Response(status=status.HTTP_400_BAD_REQUEST)
 
         except Product.DoesNotExist:
-            print('noproduct')
-            return Response(status=404)
+            return Response(status=status.HTTP_400_BAD_REQUEST)
 
         except CartItem.DoesNotExist:
-            print('nocartItem')
-            return Response(status=404)
-    return Response(status=405)
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+    return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
 @csrf_exempt
 @api_view(['GET'])
 def get_products(request):
-     try:
-         category_id = request.GET.get('category_id')
-         if category_id is None:
-             return Response(status=400)
-         category = ProductCategory.objects.get(pk=category_id)
-         products = Product.objects.filter(category=category)
-         product_data = [
-              {'name': product.name, 'price': product.price, 'stock': product.stock, 'image': product.image, 'ref': product.ref}
-               for product in products
-         ]
-         return Response(product_data)
-     except ProductCategory.DoesNotExist:
-         return Response(status=404)
+    if (request.method=="GET"):
+        try:
+            category_id = request.GET.get('category_id')
+            if category_id is None:
+                return Response(status=status.HTTP_400_BAD_REQUEST)
+            category = ProductCategory.objects.get(pk=category_id)
+            products = Product.objects.filter(category=category)
+            product_data = [
+                {'name': product.name, 'price': product.price, 'stock': product.stock, 'image': product.image, 'ref': product.ref}
+                for product in products
+            ]
+            return Response(product_data, status=status.HTTP_200_OK)
+        except ProductCategory.DoesNotExist:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+    else:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
 
 @csrf_exempt
 @api_view(['GET'])
 def get_user(request):
-     try:
-         user_id = request.GET.get('userID')
-         if user_id is None:
-             return Response(status=400)
-         user = User.objects.get(pk=user_id)
-         user_data ={'first_name': user.first_name, 'last_name': user.last_name, 'email': user.email}
-         return Response(user_data)
-     except ProductCategory.DoesNotExist:
-         return Response(status=404)
+    if (request.method=="GET"):
+        try:
+            user_id = request.GET.get('userID')
+            if user_id is None:
+                return Response(status=status.HTTP_400_BAD_REQUEST)
+            user = User.objects.get(pk=user_id)
+            user_data ={'first_name': user.first_name, 'last_name': user.last_name, 'email': user.email}
+            return Response(user_data, status=status.HTTP_200_OK)
+        except User.DoesNotExist:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+    else:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
 
 @csrf_exempt
 @api_view(['GET'])
 def get_cart(request):
-     try:
-          user_id = request.GET.get('userID')
-          if user_id is None or user_id == "null":
-             return Response({'name': 'test', 'quantity':'5', 'price': '10', 'image': 'c01.png', 'ref': "test"})
-          cart = Cart.objects.get(created_by_id=user_id)
-          cart_items = CartItem.objects.filter(cart=cart)
-          product_data = [
-                       {'name': cart_item.product.name, 'quantity':cart_item.quantity, 'price': cart_item.product.price * cart_item.quantity, 'image': cart_item.product.image, 'ref': cart_item.product.ref}
-                        for cart_item in cart_items
-                  ]
-          return Response(product_data)
-     except Cart.DoesNotExist:
-          return Response(status=404)
+    if (request.method=="GET"):
+        try:
+            user_id = request.GET.get('userID')
+            if user_id is None or user_id == "null":
+                return Response(status=status.HTTP_400_BAD_REQUEST)
+            cart = Cart.objects.get(created_by_id=user_id)
+            cart_items = CartItem.objects.filter(cart=cart)
+            product_data = [
+                {'name': cart_item.product.name, 'quantity':cart_item.quantity, 'price': cart_item.product.price * cart_item.quantity, 'image': cart_item.product.image, 'ref': cart_item.product.ref}
+                for cart_item in cart_items
+            ]
+            return Response(product_data, status=status.HTTP_200_OK)
+        except Cart.DoesNotExist:
+            return Response(status=404)
+    else:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
 
 @csrf_exempt
 @api_view(['GET'])
 def get_orders(request):
-     try:
-          user_id = request.GET.get('userID')
-          orders = orders = Order.objects.filter(placed_by_id=user_id)
-
-          orders_data = []
-          for order in orders:
-              order_data = {
-                'id': order.id,
-                'placed_at': order.placed_at.strftime('%Y-%m-%d %H:%M:%S'),  # Format the datetime as string
-                'address': order.address.__str__() if order.address else None,  # Address as string or None if not provided
-                'order_items': [{
-                    'quantity': order_item.quantity,
-                    'product_name': order_item.product.name,
-                    'product_price': order_item.product.price,
-                    'product_image': order_item.product.image,
-                    'product_ref': order_item.product.ref
-                } for order_item in order.orderitem_set.all()]  # Fetch order items for the order
-              }
-              orders_data.append(order_data)
-
-          return Response(orders_data)
-     except Cart.DoesNotExist:
-          return Response(status=404)
+    if (request.method=="GET"):
+        try:
+            user_id = request.GET.get('userID')
+            orders = orders = Order.objects.filter(placed_by_id=user_id)
+            orders_data = []
+            for order in orders:
+                order_data = {
+                    'id': order.id,
+                    'placed_at': order.placed_at.strftime('%Y-%m-%d %H:%M:%S'),  # Format the datetime as string
+                    'address': order.address.__str__() if order.address else None,  # Address as string or None if not provided
+                    'order_items': [{
+                        'quantity': order_item.quantity,
+                        'product_name': order_item.product.name,
+                        'product_price': order_item.product.price,
+                        'product_image': order_item.product.image,
+                        'product_ref': order_item.product.ref
+                    } for order_item in order.orderitem_set.all()]  # Fetch order items for the order
+                }
+                orders_data.append(order_data)
+            return Response(orders_data, status=status.HTTP_200_OK)
+        except Order.DoesNotExist:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+    else:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
 
 @csrf_exempt
 @api_view(['POST'])
 def create_order(request):
+    """request={
+        user_id
+        address_id
+    }"""
     if request.method == 'POST':
         try:
             user_id = request.data.get('user_id')
@@ -403,16 +429,15 @@ def create_order(request):
             return Response(status=200)
 
         except Cart.DoesNotExist:
-            print('noCart')
-            return Response(status=404)
+            return Response(status=status.HTTP_400_BAD_REQUEST)
 
         except Address.DoesNotExist:
-            return Response(status=404)
+            return Response(status=status.HTTP_400_BAD_REQUEST)
 
         except Exception as e:
-            print(e)
             return Response(status=500)
-    return Response(status=405)
+    else:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
 
